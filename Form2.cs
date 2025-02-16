52c7c48d48e1822974c6ab1a3c51d4997d1e9d3e
Daliene Roque <dalienenl@gmail.com>
2025-02-16 14:20:03 -0300

    Adicionado código para conectar ao MySQL e carregar registros na tabela

--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using System.Windows.Forms;
+using System.Data;
 
 namespace ImageStore
 {
@@ -7,6 +10,45 @@ namespace ImageStore
         public Form2()
         {
             InitializeComponent();
+
+            string conexao = "server=localhost; User id=root; password=; database=imagens_db;";
+            //Faz conexão com a base de dados
+            MySqlConnection conn = new MySqlConnection(conexao);
+            string sql = "SELECT * FROM tabela_imagens";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conexao);
+
+            try
+            {
+                DataTable dt = new DataTable();
+                conn.Open();
+                if (adapter.Fill(dt) < 1)
+                {
+                    labelResultado.Text = "A base de dados ainda não contém registros";
+                    labelResultado.Visible = true;
+                }
+                else
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        listaImagem.Rows.Add(dr.ItemArray);
+                    }
+
+                    btnExcluir.Enabled = true;
+                    btnSalvar.Enabled = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                labelResultado.Text = $"Erro ao recuperar os registros. \n {ex.Message}";
+                labelResultado.Visible = true;
+            }
+            finally
+            {
+                conn.Close();
+                adapter.Dispose();
+            }
+
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
